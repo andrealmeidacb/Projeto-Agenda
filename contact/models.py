70efe4341d97f9f142e3b7f36e07516cbe71b080
André Almeida <andrealmeidacb777@gmail.com>
2025-11-18 23:59:19 -0300

    Terminando de criar e configurar o ultimo campo do app Contact o owner que é uma foreing key e criando um script para gerar contatos aleatórios usando a biblioteca Faker

--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # id (primary key - automático)
 # first_name (string), last_name (string), phone (string)
@@ -8,11 +9,18 @@ from django.utils import timezone
 
 # Depois  
 # owner (foreign key)
+
+
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=50)
 
     def __str__(self) -> str:
         return self.name
+
 
 class Contact(models.Model):
     fisrt_name = models.CharField(max_length=50)
@@ -25,6 +33,11 @@ class Contact(models.Model):
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
     category = models.ForeignKey(
         Category, 
+        on_delete=models.SET_NULL, 
+        blank=True, null=True
+    )
+    owner = models.ForeignKey(
+        User, 
         on_delete=models.SET_NULL, 
         blank=True, null=True
     )
