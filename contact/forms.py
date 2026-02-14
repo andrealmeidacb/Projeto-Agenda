2985fab1d466ed8c40dd8df682f108133988e816
André Almeida <andrealmeidacb777@gmail.com>
2026-02-13 23:46:25 -0300

    Criando um formulario para atualizar dados do usuário

--- contact/forms.py
+++ contact/forms.py
@@ -3,6 +3,7 @@ from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from . import models
+from django.contrib.auth import password_validation
 
 class ContactForm(forms.ModelForm):
     picture = forms.ImageField(
@@ -41,13 +42,14 @@ class ContactForm(forms.ModelForm):
     def clean_first_name(self):
         fisrt_name = self.cleaned_data.get('fisrt_name')
 
-        msg = ValidationError(
-                'Veio do add_error',
-                code='invalid'
-            )
-
         if fisrt_name == 'ABC':
-            self.add_error('first_name', msg)
+            self.add_error(
+                'first_name',
+                ValidationError(
+                    'Veio do add_error',
+                    code='invalid'
+                ))    
+        return fisrt_name
 
 
 class RegisterForm(UserCreationForm):
@@ -80,3 +82,97 @@ class RegisterForm(UserCreationForm):
             )
 
         return email
+    
+class RegisterUpdateForm(forms.ModelForm):
+    first_name = forms.CharField(
+        min_length=2,
+        max_length=30,
+        required=True,
+        help_text='Required.',
+        error_messages={
+            'min_length': 'Please, add more than 2 letters.'
+        }
+    )
+    last_name = forms.CharField(
+        min_length=2,
+        max_length=30,
+        required=True,
+        help_text='Required.'
+    )
+
+    password1 = forms.CharField(
+        label="Password",
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+        help_text=password_validation.password_validators_help_text_html(),
+        required=False,
+    )
+
+    password2 = forms.CharField(
+        label="Password 2",
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+        help_text='Use the same password as before.',
+        required=False,
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'email',
+            'username',
+        )
+
+    def save(self, commit=True):
+        cleaned_data = self.cleaned_data
+        user = super().save(commit=False)
+
+        password = cleaned_data.get('password1')
+
+        if password:
+            user.set_password(password)
+
+        if commit:
+            user.save()
+
+        return user
+
+    def clean(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        if password1 or password2:
+            if password1 != password2:
+                self.add_error(
+                    'password2',
+                    ValidationError('Senhas não batem')
+                )
+
+        return super().clean()
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        current_email = self.instance.email
+
+        if current_email != email:
+            if User.objects.filter(email=email).exists():
+                self.add_error(
+                    'email',
+                    ValidationError('Já existe esse e-mail', code='invalid')
+                )
+
+        return email
+    
+    def clean_password1(self):
+        password1 = self.cleaned_data.get('password1')
+
+        if password1:
+            try:
+                password_validation.validate_password(password1)
+            except ValidationError as errors:
+                self.add_error(
+                    'password1',
+                    ValidationError(errors)
+                )
+        
+        return password1
