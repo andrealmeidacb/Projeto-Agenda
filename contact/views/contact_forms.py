d3a32c93817084b7093016f862e7befefcfcdbd8
André Almeida <andrealmeidacb777@gmail.com>
2026-02-16 20:30:35 -0300

    login_required - garantindo que o usuário está logado para acessar view

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from contact.forms import ContactForm
 from contact.models import Contact
+from django.contrib.auth.decorators import login_required
+
 
 
 def create(request):
@@ -36,7 +38,7 @@ def create(request):
         context,
     )
 
-
+@login_required(login_url='contact:login')
 def update(request, contact_id):
     contact = get_object_or_404(
         Contact, pk=contact_id, show=True
@@ -76,6 +78,7 @@ def update(request, contact_id):
         context,
     )
 
+@login_required(login_url='contact:login')
 def delete(request, contact_id):
     contact = get_object_or_404(
         Contact, pk=contact_id, show=True
